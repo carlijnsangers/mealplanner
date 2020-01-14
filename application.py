b1ef33164c3d2cf968460d0a53a0522df4fb1b67
Martijn <mh107258@gmail.com>
2020-01-14 10:06:59 +0000

    application.py

--- application.py
+++ application.py
@@ -31,7 +31,9 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
-
+@app.route("/")
+def hallo():
+    return "Hallo"
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -45,22 +47,22 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return #apology("must provide username", 403)
 
         # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
+        elif not #request.form.get("password"):
+            return #apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          username=request.form.get("username"))
+        #rows = db.execute("SELECT * FROM users WHERE username = :username",
+        #                  username=request.form.get("username"))
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+        #if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        #    return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        #session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
         flash('Logged in')
@@ -104,19 +106,19 @@ def register():
 
         # Checks if username is not taken
         username = request.form.get("username")
-        rows =  db.execute("SELECT * FROM users WHERE username = %s", username)
-        print(len(rows))
-        if len(rows) >= 1:
-            return apology("username is already taken", 400)
+        #rows =  db.execute("SELECT * FROM users WHERE username = %s", username)
+        #print(len(rows))
+        #if len(rows) >= 1:
+        #    return apology("username is already taken", 400)
 
         # Puts username(UN) and password(PW) in database
         else:
             PW = generate_password_hash(request.form.get("password"))
-            db.execute("INSERT INTO users (username, hash) VALUES (%s, %s)", (username, PW))
+            #db.execute("INSERT INTO users (username, hash) VALUES (%s, %s)", (username, PW))
 
             # Remember which user has logged in
-            session["user_id"] = db.execute("SELECT id FROM users WHERE username = :username",
-                          username=request.form.get("username"))[0]['id']
+            #session["user_id"] = db.execute("SELECT id FROM users WHERE username = :username",
+            #              username=request.form.get("username"))[0]['id']
 
             flash('Registered')
             return redirect("/")
