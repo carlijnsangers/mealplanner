20a586b91e0068fb3cd0c94eafa67b4439dbe00a
Martijn <mh107258@gmail.com>
2020-01-14 13:00:50 +0000

    db, login, register. Login nog error

--- application.py
+++ application.py
@@ -30,12 +30,15 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# test database
+db = SQL("sqlite:///test.db")
 
 @app.route("/")
 def hallo():
     flash("hello")
     return render_template("layout.html")
 
+# geeft momenteel error met de huidige login pagina
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
@@ -48,22 +51,22 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return #apology("must provide username", 403)
+            return 'error' #apology("must provide username", 403)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return #apology("must provide password", 403)
+            return 'error' #apology("must provide password", 403)
 
         # Query database for username
-        #rows = db.execute("SELECT * FROM users WHERE username = :username",
-        #                  username=request.form.get("username"))
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
 
         # Ensure username exists and password is correct
-        #if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-        #    return apology("invalid username and/or password", 403)
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return 'error' #apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
-        #session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
         flash('Logged in')
@@ -96,29 +99,29 @@ def register():
 
         # Checks if username/password is given
         if not request.form.get("username"):
-            return apology("must provide username", 400)
+            return #apology("must provide username", 400)
         elif not request.form.get("password"):
-            return apology("must provide password", 400)
+            return #apology("must provide password", 400)
 
         # Checks if passwords match
         elif request.form.get("password") != request.form.get("confirmation"):
-            return apology("passwords do not match", 400)
+            return #apology("passwords do not match", 400)
 
         # Checks if username is not taken
         username = request.form.get("username")
-        #rows =  db.execute("SELECT * FROM users WHERE username = %s", username)
+        rows =  db.execute("SELECT * FROM users WHERE username = %s", username)
         #print(len(rows))
-        #if len(rows) >= 1:
-        #    return apology("username is already taken", 400)
+        if len(rows) >= 1:
+            return "error"  #apology("username is already taken", 400)
 
         # Puts username(UN) and password(PW) in database
-        # else:
-        PW = generate_password_hash(request.form.get("password"))
-            #db.execute("INSERT INTO users (username, hash) VALUES (%s, %s)", (username, PW))
+        else:
+            PW = generate_password_hash(request.form.get("password"))
+            db.execute("INSERT INTO users (username, hash) VALUES (%s, %s)", (username, PW))
 
-            # Remember which user has logged in
-            #session["user_id"] = db.execute("SELECT id FROM users WHERE username = :username",
-            #              username=request.form.get("username"))[0]['id']
+            #Remember which user has logged in
+            session["user_id"] = db.execute("SELECT id FROM users WHERE username = :username",
+                          username=request.form.get("username"))[0]['id']
 
         flash('Registered')
         return redirect("/")
