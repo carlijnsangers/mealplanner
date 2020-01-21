7a9b9c96c8abdc1f55bdcbd842dcc688ba92aeaf
Sander <sander.oud1999@gmail.com>
2020-01-21 09:22:45 +0000

    query

--- helpers.py
+++ helpers.py
@@ -5,15 +5,13 @@ import os
 # link naar rapidapi met voorgeprogrammeerde functies
 # https://goo.gl/9XTKTZ
 
-def lookup(diet, intolerances):
-    response = requests.get(f"https://api.spoonacular.com/food/products/search?query=Burger&instructionsRequired=true&intolerances={(intolerances)}&diet={(diet)}&number=5&ins&apiKey=966dd37a9cba411dbdf6c4d9c5575c64")
-
+def lookup(query, diet, intolerances):
+    response = requests.get(f"https://api.spoonacular.com/food/products/search?query={()}&instructionsRequired=true&intolerances={(intolerances)}&diet={(diet)}&number=5&ins&apiKey=966dd37a9cba411dbdf6c4d9c5575c64")
     quote = response.json()
     return {
         "meal": quote["products"][0]["title"],
         "image": quote["products"][0]["image"]
-
     }
 
 
-print(lookup(None, ["Dairy", "Egg"]))
+print(lookup("rice", None, ["Dairy", "Egg"]))
