c906942c71507ff0f4ebe104130fc01e7ed75033
Sander <sander.oud1999@gmail.com>
2020-01-21 13:26:02 +0000

    meal display

--- helpers.py
+++ helpers.py
@@ -10,9 +10,9 @@ def get_meal(query, diet, intolerances):
     response = requests.get(f"https://api.spoonacular.com/recipes/complexSearch?query={(query)}&course&instructionsRequired=true&intolerances={(intolerances)}&diet={(diet)}&number=5&apiKey=966dd37a9cba411dbdf6c4d9c5575c64")
     quote = response.json()
     # choice = random.randrange(0, 5)
+    print(quote)
     return {
         "id": quote["results"][0]["id"],
         "meal": quote["results"][0]["title"],
         "image": quote["results"][0]["image"]
-
     }
