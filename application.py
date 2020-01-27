d1896bc9d2bfa3368fe7c636e8d77a7884f9707e
Sander <sander.oud1999@gmail.com>
2020-01-27 12:53:49 +0000

    menu redirect

--- application.py
+++ application.py
@@ -208,7 +208,7 @@ def profile():
         user_id = get_user()
         voorkeuren = preferences(user_id)
         print(voorkeuren)
-        favorites = db.execute("SELECT * FROM favorite WHERE user_id=:user_id", user_id=user_id)
+        favorites = db.execute("SELECT * FROM favorites WHERE user_id=:user_id", user_id=user_id)
         return render_template("profile.html", voorkeuren=voorkeuren)
     else:
         return render_template("profile.html")
