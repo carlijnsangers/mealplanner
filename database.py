05e19f10e91f2913211c75d8eb05761893e3a936
Martijn <mh107258@gmail.com>
2020-01-27 09:37:57 +0000

    database.py

--- database.py
+++ database.py
@@ -2,3 +2,9 @@
 # gebruiken met database.functie
 
 from CS50 import SQL
+db = SQL("sqlite:///test.db")
+
+# user in db
+def user_in_db(username):
+    data = db.execute("SELECT * FROM users WHERE username=:username", username=username)
+    return data
